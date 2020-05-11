cdf52d8bb65df5a92fca9d95db1d50ecf0b91582
Avinash Reddy Desireddy <avinashr.desireddy@gmail.com>
2020-05-10 23:21:51 -0500

    Reverse a string

--- module01/reverse.go
+++ module01/reverse.go
@@ -1,11 +1,28 @@
 package module01
 
+import "strings"
 // Reverse will return the provided word in reverse
 // order. Eg:
 //
 //   Reverse("cat") => "tac"
 //   Reverse("alphabet") => "tebahpla"
 //
+/*
 func Reverse(word string) string {
-	return ""
+    var reverse string
+    for i := len(word) - 1 ; i >= 0; i-- {
+        reverse = reverse + string(word[i])
+    }
+	return reverse
 }
+*/
+
+// Reverse using string builder
+func Reverse(word string) string {
+    var sb strings.Builder
+    for i := len(word) - 1; i >= 0; i-- {
+        sb.WriteByte(word[i])
+    }
+    return sb.String()
+}
+
